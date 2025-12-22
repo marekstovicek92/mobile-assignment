03b1f73a3063d1516860a2a363ba581574687a92
Marek Šťovíček <marek.stovicek@seznam.cz>
2025-12-22 18:40:55 +0100

    Initial commit

---
+++ SpaceX/SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Marek Šťovíček on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpaceX/SpaceXTests/SpaceXTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXTests.swift
+//  SpaceXTests
+//
+//  Created by Marek Šťovíček on 22.12.2025.
+//
+
+import Testing
+@testable import SpaceX
+
+struct SpaceXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
